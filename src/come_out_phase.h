940206716e29c2b57911a53b08342775ef1c089b
valdovinben <143145543+valdovinben@users.noreply.github.com>
2023-12-09 19:01:12 +0000

    Question 4

--- src/come_out_phase.h
+++ src/come_out_phase.h
@@ -1,1 +1,11 @@
-//
+
+#include "phase.h"
+
+class Roll;
+
+class ComeOutPhase : public Phase {
+public:
+    RollOutcome get_outcome(Roll* roll) override;
+};
+
+
--- src/point_phase.h
+++ src/point_phase.h
@@ -1,1 +1,16 @@
-//
+
+
+#include "phase.h"
+
+class Roll;
+
+class PointPhase : public Phase {
+private:
+    int point;
+
+public:
+    PointPhase(int p);
+    RollOutcome get_outcome(Roll* roll) override;
+};
+
+
